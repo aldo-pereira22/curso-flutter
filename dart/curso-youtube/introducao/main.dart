f2a986bc05bd86f153ae9070feef94753f2f4f84
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2021-11-04 20:25:26 -0300

    Criação do projeto

---
+++ dart/curso-youtube/introducao/main.dart
@@ -0,0 +1,14 @@
+main() {
+  int numero = 5;
+  if (numero % 2 == 0) {
+    print("Este número é par: $numero");
+  } else {
+    print("Este número é impar: $numero");
+  }
+
+  var lista = [1, 2, 3];
+
+  if (lista.contains(2)) {
+    print("A lista contém o valor 2");
+  }
+}
