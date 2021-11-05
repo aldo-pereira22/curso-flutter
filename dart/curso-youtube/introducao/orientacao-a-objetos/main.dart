0c922fa46b9db1919c87c4ec12868df33a88ede3
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2021-11-04 21:08:54 -0300

    Entendo a composição de classes no Dart

--- dart/curso-youtube/introducao/orientacao-a-objetos/main.dart
+++ dart/curso-youtube/introducao/orientacao-a-objetos/main.dart
@@ -1,10 +1,42 @@
+import 'cartao.dart';
+import 'cliente.dart';
+import 'compra.dart';
+import 'conta.dart';
+import 'fatura.dart';
 import 'pessoa.dart';
 
 main() {
-  Pessoa joao = Pessoa(
-    nome: "Aldo",
-    sobrenome: 'Pereira',
+  var conta = Conta(
+    cliente: Cliente(
+      id: "12345",
+      cpf: "023.323.234-44",
+      nome: "Aldo",
+      sobrenome: "Pereira",
+    ),
+    cartao: Cartao(
+      numero: "123456",
+      limite: 12500,
+      mes: 06,
+      ano: 2025,
+      codigo: 123,
+    ),
+    faturas: [
+      Fatura(
+        compras: [
+          Compra(
+            valor: 12.5,
+            descricao: "Café",
+            data: "12/06",
+          ),
+          Compra(
+            valor: 259.9,
+            descricao: "Chuteira",
+            data: "04/11/2021",
+          ),
+        ],
+        mes: 06,
+        ano: 2021,
+      )
+    ],
   );
-
-  print("O nome completo é: ${joao.nomeCompleto}");
 }
