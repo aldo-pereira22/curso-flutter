2ec3f91cbf8e7eeb7b1e76ec02f554d98f8e5d06
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2021-12-02 16:10:15 -0300

    Atualizaçao

---
+++ flutter/flutter_aula_1/lib/meuaplicativo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
